a62353ef2f7b89df64fed2b2297e36abe20cde12
lujan82 <lujanbenita@gmail.com>
2022-02-20 20:34:33 +0100

    fixed infoCoin

--- src/components/crypto/infoCoin.jsx
+++ src/components/crypto/infoCoin.jsx
@@ -1,17 +1,17 @@
-import { numberWithCommas } from "../../utils/numberWithCommas";
+import { formatNumberThousands } from '../../utils/formatNumbers'
 
-const InfoCoin = ({dataCoin}) => {
+const InfoCoin = ({ dataCoin }) => {
   return (
-    <div className="crypto__head">
-      <img src={dataCoin.image.small} alt="symbol"/>
-      <div className="crypto__titles">
-        <span className="crypto__title">{dataCoin.name}</span>
-        <span className="crypto__id">{dataCoin.symbol}</span>
+    <div className='crypto__head'>
+      <img src={dataCoin.image.small} alt='symbol' />
+      <div className='crypto__titles'>
+        <span className='crypto__title'>{dataCoin.name}</span>
+        <span className='crypto__id'>{dataCoin.symbol}</span>
       </div>
-      <div className="crypto__price">{numberWithCommas(dataCoin.market_data.current_price.eur)} €</div>
-      <div className="crypto__rank">Rank: {dataCoin.coingecko_rank}</div>
+      <div className='crypto__price'>{formatNumberThousands(dataCoin.market_data.current_price.eur)} €</div>
+      <div className='crypto__rank'>Rank: {dataCoin.coingecko_rank}</div>
     </div>
-  );
-};
+  )
+}
 
-export default InfoCoin;
+export default InfoCoin
